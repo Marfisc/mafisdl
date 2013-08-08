459f1ee8ec6393d825cddfc2ec2e0390d953c8cd
Marcel Fischer <ich@mar-cel.eu>
2013-08-08 15:56:32 +0200

    Added better event listener

--- events.d
+++ events.d
@@ -31,6 +31,88 @@ mixin aliasEvent!("UserEvent");
 mixin aliasEvent!("SysWMEvent");
 alias SDL_Event Event;
 
+struct KeyUp {
+    KeyboardEvent event;
+    alias event this;
+}
+struct KeyDown {
+    KeyboardEvent event;
+    alias event this;
+}
+
+struct MouseButtonUp {
+    MouseButtonEvent event;
+    alias event this;
+}
+struct MouseButtonDown {
+    MouseButtonEvent event;
+    alias event this;
+}
+
+struct JoyButtonUp {
+    JoyButtonEvent event;
+    alias event this;
+}
+struct JoyButtonDown {
+    JoyButtonEvent event;
+    alias event this;
+}
+
+private KeyUp eventKeyUp(Event ev) { return KeyUp(ev.key); }
+private KeyDown eventKeyDown(Event ev) { return KeyDown(ev.key); }
+private MouseButtonUp eventMouseButtonUp(Event ev) { return MouseButtonUp(ev.button); }
+private MouseButtonDown eventMouseButtonDown(Event ev) { return MouseButtonDown(ev.button); }
+private JoyButtonUp eventJoyButtonUp(Event ev) { return JoyButtonUp(ev.jbutton); }
+private JoyButtonDown eventJoyButtonDown(Event ev) { return JoyButtonDown(ev.jbutton); }
+
+void eventLoop(T)(T that) {
+    template caseOnEvent(string constant, string name) {
+        enum caseOnEvent = "
+        static if(is(typeof(that.on(polledEvent."~name~")))) {
+            case "~constant~":
+            that.on(polledEvent."~name~"); break;
+        }";
+    }
+
+    static if(is(typeof(that.beginLoop()))) {
+        that.beginLoop();
+    }
+
+
+    while(mixin(is(typeof(that.keepLooping))?
+        "that.keepLooping" : "true")) {
+        SDL_Event polledEvent;
+        while(SDL_PollEvent(&polledEvent)) {
+            polledEvent.eventKeyUp;
+            switch(polledEvent.type) {
+                mixin(caseOnEvent!("SDL_QUIT", "quit"));
+                mixin(caseOnEvent!("SDL_ACTIVEEVEENT", "active"));
+                mixin(caseOnEvent!("SDL_KEYDOWN", "eventKeyDown"));
+                mixin(caseOnEvent!("SDL_KEYUP", "eventKeyUp"));
+                mixin(caseOnEvent!("SDL_MOUSEMOTION", "motion"));
+                mixin(caseOnEvent!("SDL_MOUSEBUTTONUP", "eventMouseButtonUp"));
+                mixin(caseOnEvent!("SDL_MOUSEBUTTONDOWN", "eventMouseButtonDown"));
+                mixin(caseOnEvent!("SDL_JOYAXISMOTION", "jaxis"));
+                mixin(caseOnEvent!("SDL_JOYBALLMOTION", "jball"));
+                mixin(caseOnEvent!("SDL_JOYHATMOTION", "jhat"));
+                mixin(caseOnEvent!("SDL_JOYBUTTONDOWN", "eventJoyButtonDown"));
+                mixin(caseOnEvent!("SDL_JOYBUTTONUP", "eventJoyButtonUp"));
+                mixin(caseOnEvent!("SDL_VIDEOEXPOSE", "expose"));
+                mixin(caseOnEvent!("SDL_VIDEORESIZE", "resize"));
+                mixin(caseOnEvent!("SDL_USEREVENT", "user"));
+                mixin(caseOnEvent!("SDL_SYSWMEVENT", "syswm"));
+            default: //has to be there even if empty
+                static if(is(typeof(that.onOther(Event.init)))) {
+                    that.onOther(polledEvent); break;
+                }
+            }
+        }
+        static if(is(typeof(that.looping()))) {
+            that.looping();
+        }
+    }
+}
+
 /*
  Internal structure for holding delegates 
  and giving them logic for listen
